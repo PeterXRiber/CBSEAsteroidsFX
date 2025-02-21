92ad6ae6d28bc435a40aa03da9b76f1a6824a8cb
PeterXRiber <p.Eisler@hotmail.dk>
2025-02-21 11:49:55 +0100

    Adding Enemy - with the help of Mads the champ

---
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
@@ -0,0 +1,42 @@
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+import java.util.Random;
+
+public class EnemyPlugin implements IGamePluginService {
+
+    private Entity enemy;
+
+    public EnemyPlugin(){
+    }
+
+    @Override
+    public void start(GameData gameData, World world) {
+
+        // Add entities to the world
+        enemy = createEnemyShip(gameData);
+        world.addEntity(enemy);
+
+    }
+
+    private Entity createEnemyShip(GameData gameData) {
+
+        Entity enemyShip = new Enemy();
+        enemyShip.setPolygonCoordinates(-5,-5,10,0,-5,5);
+        enemyShip.setX(gameData.getDisplayHeight()/4);
+        enemyShip.setY(gameData.getDisplayWidth()/4);
+        enemyShip.setRadius(8);
+        return enemyShip;
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        // Remove entities
+        world.removeEntity(enemy);
+
+    }
+}
