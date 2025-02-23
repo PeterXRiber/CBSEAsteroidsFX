eb48b714e070149707abe61bedae7cb4068a85bd
PeterXRiber <p.Eisler@hotmail.dk>
2025-02-23 17:52:55 +0100

    Added the updated version of the game - be ready to revert back to last commit, if you get lost

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/data/Entity.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/data/Entity.java
@@ -1,18 +1,52 @@
 package dk.sdu.mmmi.cbse.common.data;
 
+
+import dk.sdu.mmmi.cbse.common.data.entityparts.EntityPart;
+
 import java.io.Serializable;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Entity implements Serializable {
 
     private final UUID ID = UUID.randomUUID();
     
     private double[] polygonCoordinates;
-    private double x;
-    private double y;
+
+    private float[] shapeX = new float[4];
+    private float[] shapeY = new float[4];
+
+
     private double rotation;
     private float radius;
-            
+
+    private Map<Class, EntityPart> parts;
+
+    public Entity() {
+        parts = new ConcurrentHashMap<>();
+    }
+
+
+    public void add(EntityPart part) {
+        parts.put(part.getClass(), part);
+    }
+
+    public void remove(Class partClass) {
+        parts.remove(partClass);
+    }
+
+    public <E extends EntityPart> E getPart(Class partClass) {
+        return (E) parts.get(partClass);
+    }
+
+    public void setRadius(float r) {
+        this.radius = r;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
 
     public String getID() {
         return ID.toString();
@@ -26,38 +60,28 @@ public class Entity implements Serializable {
     public double[] getPolygonCoordinates() {
         return polygonCoordinates;
     }
-       
 
-    public void setX(double x) {
-        this.x =x;
+
+    public float[] getShapeX() {
+        return shapeX;
     }
 
-    public double getX() {
-        return x;
+    public void setShapeX(float[] shapeX) {
+        this.shapeX = shapeX;
     }
 
-    
-    public void setY(double y) {
-        this.y = y;
+    public float[] getShapeY() {
+        return shapeY;
     }
 
-    public double getY() {
-        return y;
+    public void setShapeY(float[] shapeY) {
+        this.shapeY = shapeY;
     }
-
     public void setRotation(double rotation) {
         this.rotation = rotation;
     }
 
     public double getRotation() {
         return rotation;
-    }
-
-    public void setRadius(float radius) {
-        this.radius = radius;
-    }
-        
-    public float getRadius() {
-        return this.radius;
     }
 }
--- Common/src/main/java/dk/sdu/mmmi/cbse/common/data/GameData.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/data/GameData.java
@@ -1,13 +1,41 @@
 package dk.sdu.mmmi.cbse.common.data;
 
+import dk.sdu.mmmi.cbse.common.events.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class GameData {
 
     // Changes the window-size
     // Changing the size impacts the player to freeze
     // Find solution later
-    private int displayWidth  = 800 ;
-    private int displayHeight = 800;
+
+    private float delta;
+    private int displayWidth;
+    private int displayHeight;
     private final GameKeys keys = new GameKeys();
+    private List<Event> events = new CopyOnWriteArrayList<>();
+
+    public void addEvent(Event e) {
+        events.add(e);
+    }
+    public void removeEvent(Event e) {
+        events.remove(e);
+    }
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setDelta(float delta) {
+        this.delta = delta;
+    }
+    public float getDelta() {
+        return delta;
+    }
+
+
 
 
     public GameKeys getKeys() {
@@ -30,5 +58,14 @@ public class GameData {
         return displayHeight;
     }
 
+    public <E extends Event> List<Event> getEvents(Class<E> type, String sourceID) {
+        List<Event> r = new ArrayList();
+        for (Event event : events) {
+            if (event.getClass().equals(type) && event.getSource().getID().equals(sourceID)) {
+                r.add(event);
+            }
+        }
+        return r;
+    }
 
 }
